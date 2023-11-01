71587f01c3f0e3279e88afe12d7d66c7d8db3556
anhcucunpro22 <ducchuy093@gmail.com>
2023-11-01 10:41:04 +0700

    Update sp3

---
+++ PCMS/Controllers/EquipmentTypesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PCMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentTypesController : ControllerBase
+    {
+    }
+}
